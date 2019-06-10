745f6f430189752083067319ffeef6d6c03b978c
javier <javigandia99@gmail.com>
2019-06-10 11:17:06 +0200

    eee

--- perfil.php
+++ perfil.php
@@ -6,42 +6,7 @@ if (!isset($_SESSION['usr'])) {
 <?php
 include 'arriba.php';
 ?>
-<div class="navbar-collapse collapse" id="navb">
-    <ul class="navul navbar-nav">
-        <li class="navText nav-item d-flex align-items-start">
-            <a class="nav-link " href="index.php">Bienvenida</a>
-        </li>
-        <li class="navText nav-item">
-            <a class="nav-link" href="secun.php">Secundaria</a>
-        </li>
-    </ul>
 
-    <ul class="nav justify-content-center">
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-    </ul>
-
-
-    <ul class="navul navbar-nav nav justify-content-end">
-        <li class="navText nav-item">
-            <a class="nav-link" href="inicio.php">Iniciar Sesion</a>
-        </li>
-        <li class="navText nav-item">
-            <a class="nav-link" href="#">Carrito</a>
-        </li>
-
-        <li class="navText nav-item">
-            <a class="nav-link disabled" href="#">Administracion</a>
-        </li>
-        <li class="navText nav-item d-flex align-items-end">
-            <a class="nav-link" href="cerrar.php">Cerrar Sesion</a>
-        </li>
-    </ul>
-</div>
 
 <?php
 include 'abajo.php';
