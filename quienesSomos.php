e818863d3ea31b917ff6fe30012645f21dacccef
javier <javigandia99@gmail.com>
2019-06-12 23:38:49 +0200

    Quienes somos acabado  y bbdd subida

--- quienesSomos.php
+++ quienesSomos.php
@@ -5,14 +5,14 @@ include 'arriba.php';
 <main class="panel-body">
   <div id="products" class="d-flex flex-row justify-content-center flex-wrap">
     <div class="item col-lg-5 cajas">
-      
-      <p>Denver es una marca de ropa nacida de un proyecto Web realizado por varios alumnos de primer curso del Grado Superior de Desarrollo de Aplicaciones Multiplataforma en el Centro Universitario U-tad de Las Rozas de Madrid. <br> <br>
-      Con esta marca hemos tratado de hacer que la gente se sienta cómoda con su ropa sin importarle lo que digan los demás. En Denver buscamos que un cliente sea él mismo y no tenga miedo a ser alternativo. <br> <br>
-      Tratamos de que cada cual pueda ajustarse a sus gustos y a su nivel adquisitivo sin pensar en diferenciar posibles clientes. A Javier Gandía le encantan los abrigos, por ejemplo y de esos habrán muchos en esta tienda. <br> <br>
-      En caso de que queráis estar más en contacto con nosotros, siguenos en todas las redes sociales existentes y si te sientes lo suficientemente cómodo contigo mismo, mencionanos en una publicación y quizás te demos un suculento descuento. Todo sea por nuestros consumidores. <br> <br>
-      Tú, no sé qué más poner aquí, se me están acabando las ideas, supongo que a Jaime tampoco le va a preocupar mucho de cara a ponernos la nota, es más me sorprendería en exceso que esté leyendo esto lmao. Quizás pueda poner algún chiste o algo. Pero solo me sé el de mis tetas así que acabaré con un Lorem ipsum que para eso estudié Latín. <br> <br>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quisquam libero id, ratione consequuntur ea, atque dicta sint pariatur animi temporibus natus eaque ipsa eveniet veniam sed enim quidem! Aliquam! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga, libero est sed maxime aut, sequi assumenda iure ad rerum nemo exercitationem odit eveniet laudantium autem cum, explicabo deleniti. Molestiae, impedit.</p>
-      
+      <div class="col-10 mt-5 mb-2">
+        <p class="text text-justify"><strong>Denver</strong> es una marca de ropa nacida de un <strong>proyecto Web</strong> realizado por varios alumnos de primer curso del Grado Superior de Desarrollo de Aplicaciones Multiplataforma en el Centro Universitario U-tad de Las Rozas de Madrid. <br> <br>
+          Con esta marca hemos tratado de hacer que la gente se sienta cómoda con su ropa sin importarle lo que digan los demás. En <strong>Denver</strong> buscamos que un cliente sea él mismo y no tenga miedo a ser alternativo. 
+          Tratamos de que cada cual pueda ajustarse a sus gustos y a su nivel adquisitivo sin pensar en diferenciar posibles clientes. A Javier Gandía le encantan los abrigos, por ejemplo y de esos habrán muchos en esta tienda. <br>
+          En caso de que queráis estar más en contacto con nosotros, siguenos en todas las redes sociales existentes y si te sientes lo suficientemente cómodo contigo mismo, mencionanos en una publicación y quizás te demos un suculento descuento. Todo sea por nuestros consumidores. <br> <br>
+          Tú, no sé qué más poner aquí, se me están acabando las ideas, supongo que a Jaime tampoco le va a preocupar mucho de cara a ponernos la nota, es más me sorprendería en exceso que esté leyendo esto lmao. Quizás pueda poner algún chiste o algo. Pero solo me sé el de mis tetas así que acabaré con un Lorem ipsum que para eso estudié Latín. <br> <br>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quisquam libero id, ratione consequuntur ea, atque dicta sint pariatur animi temporibus natus eaque ipsa eveniet veniam sed enim quidem! Aliquam! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga, libero est sed maxime aut, sequi assumenda iure ad rerum nemo exercitationem odit eveniet laudantium autem cum, explicabo deleniti. Molestiae, impedit.</p>
+      </div>
     </div>
     <div class="item col-lg-5 cajas" id="somos">
 
